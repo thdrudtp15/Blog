9b4226c6a5375f6c9d0b859acff69b5243ce9113
thdrudtp15 <15rudtp@naver.com>
2025-12-24 14:52:17 +0900

    feat: 자격증_정보처리기사 추가

--- containers/Others.tsx
+++ containers/Others.tsx
@@ -32,10 +32,10 @@ const Others = () => {
                         <Item title="중부대학교" detail="편입(졸업)" period="2020.03 - 2023.2" />
                     </div>
                 </div>
-                {/* <div className={styles.section}>
+                <div className={styles.section}>
                     <h3 className={styles.section_title}>자격증</h3>
-                    <Item title="정보처리기사" detail="한국산업인력공단"  />
-                </div> */}
+                    <Item title="정보처리기사" detail="한국산업인력공단"  period='2025.12'/>
+                </div>
                 <div className={styles.section}>
                     <h3 className={styles.section_title}>교육 이수</h3>
                     <Item title="멀티캠퍼스" detail="채용연계 풀스택 개발자 부트캠프(스프링&리액트)" period="2023.01 - 2023.06" />
